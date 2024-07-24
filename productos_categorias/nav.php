3ec7b1c852c6ae603ec33c36aacc3955bfb54c97
joselgraterol <97045794+joselgraterol@users.noreply.github.com>
2024-07-24 10:54:47 -0400

    Delete productos_categorias directory

--- productos_categorias/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-	<ul>
-		<li><a href="categories.php">add category</a></li>
-		<li><a href="products.php">add product</a></li>
-		<li><a href="all_products.php">all products</a></li>
-		<li><a href="all_products_filter.php">all products filter</a></li>
-	</ul>
-</nav>
